b250b109200b54ac1eef73d6a3d04e2785a0b43c
scholar <sokolov.feliks@gmail.com>
2022-12-21 12:23:06 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+import json
+from flask import Flask
+
+
+def get_valutes_list():
+    url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    valutes = list(data['Valute'].values())
+    return valutes
+
+print(get_valutes_list())
+
+app = Flask(__name__)
+
+
+def create_html(valutes):
+    text = '<h1>Курс валют</h1>'
+    text += '<table>'
+
+    # БЫЛО
+    # text += '<tr>'
+    # for _ in valutes[0]:
+    #     text += f'<th><th>'
+    # text += '</tr>'
+    # /БЫЛО
+
+    # СТАЛО
+    text += '<tr>'
+    for _ in valutes[0].keys():
+        text += f'<th>{_}</th>'
+    text += '</tr>'
+    # /СТАЛО
+
+    for valute in valutes:
+        text += '<tr>'
+        for v in valute.values():
+            text += f'<td>{v}</td>'
+        text += '</tr>'
+
+    text += '</table>'
+    return text
+
+
+@app.route("/")
+def index():
+    valutes = get_valutes_list()
+    html = create_html(valutes)
+    return html
+
+
+if __name__ == "__main__":
+    app.run()
